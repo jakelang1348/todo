77cf98dc30b558f6dedd67b45cfebe07c249d125
jakelang <jakelang@umich.edu>
2024-07-03 20:09:01 -0500

    progress, but running into event listener problem

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const ENTER = 'e';
 const CLICK = 'c';
+let items= [];
+let count=0;
 
 
 
@@ -9,13 +11,30 @@ let addBtn = document.getElementById('addBtn');
 let submitHandler = (event) => {
     if (event.keyCode === 13) {
         const userInput = document.getElementById('userInput').value;
-        alert(userInput);
+        addCode(userInput);
     }
 }
 
 let submitHandlerClick = () => {
     const userInput = document.getElementById('userInput').value;
-    alert(userInput);
+    addCode(userInput);
+}
+
+let deleteCode = (id) => {
+    // let codeToDelete = document.getElementById(id);
+    // codeToDelete.parentNode.removeChild;
+    alert(id);
+}
+
+let addCode = (item) => {
+    //add new code
+    let codeToAdd = `<li id=newCode${count}>${item} <input type="checkbox" id="item${count}"></li>`;
+    document.getElementById('list').innerHTML += codeToAdd;   
+
+    //housekeeping for deletion
+    const checkBoxBtn = document.getElementById(`item${count}`);
+    checkBoxBtn.addEventListener('click', deleteCode.bind(null, `newCode${count}`));
+    count++; //incr count to keep ids unique
 }
 
 
